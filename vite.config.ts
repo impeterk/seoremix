9a2f3709d6299248b3802b8db08b5ba53b2af75c
Peter <storm.sk123@gmail.com>
2024-10-11 00:23:02 +0200

    basic hono added

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 
-import devServer, { defaultOptions } from "@hono/vite-dev-server";
-import adapter from "@hono/vite-dev-server/node";
+import { devServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [
+    devServer(),
     remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
-    }),
-    devServer({
-      adapter,
-      entry: "server/index.ts",
-      exclude: [...defaultOptions.exclude, "/assets/**", "/app/**"],
-      injectClientScript: false,
     }),
     tsconfigPaths(),
   ],
